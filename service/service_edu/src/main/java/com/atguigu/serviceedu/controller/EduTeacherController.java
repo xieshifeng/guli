470789047389f55f85fc3eaf9aefe1dd6389bc1b
root <565978641@qq.com>
2021-09-22 18:26:22 +0800

    hotfix_day02_v0.3

---
+++ service/service_edu/src/main/java/com/atguigu/serviceedu/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.atguigu.serviceedu.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author testjava
+ * @since 2021-09-22
+ */
+@RestController
+@RequestMapping("/serviceedu/edu-teacher")
+public class EduTeacherController {
+
+}
+
---
+++ service/service_edu/src/main/java/com/atguigu/serviceedu/service/EduTeacherService.java
@@ -0,0 +1,16 @@
+package com.atguigu.serviceedu.service;
+
+import com.atguigu.serviceedu.entity.EduTeacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2021-09-22
+ */
+public interface EduTeacherService extends IService<EduTeacher> {
+
+}
